63da7c49145e1f7ec5c5970798aea784bcc8d599
LanceBalstad <125516589+LanceBalstad@users.noreply.github.com>
2024-09-20 08:13:41 -0700

    Starting work on Header by adding logout feature to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import "./App.css";
 
 function App() {
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
   return (
     <>
       <div>App</div>
+      <button onClick={logout}>Logout</button>
     </>
   );
 }
