257aa0df5b981b1ab355a1d74c6476de2ad8dd5d
LanceBalstad <125516589+LanceBalstad@users.noreply.github.com>
2024-10-22 22:27:48 +0000

    Cleaning up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
-import Import_Products from "./components/Import_Products/Import_Products";
 import { Router } from "./Router";
 
 function App() {
   return (
     <>
       <RouterProvider router={Router} />
-      <Import_Products />
     </>
   );
 }
