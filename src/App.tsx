62b27e9c9873414bbd3c2c8477a57ed5009e76e9
Lance Balstad <Lance.balstad@ndsu.edu>
2024-10-21 21:29:54 -0500

    Import products functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Import_Products from "./components/Import_Products/Import_Products";
 
 function App() {
   return (
     <>
       <div>App</div>
+      <Import_Products />
+      <div>here</div>
     </>
   );
 }
